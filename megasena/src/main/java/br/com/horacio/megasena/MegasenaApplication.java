cf25b58f8f577240a4cf4351cb38a636f1a449b5
izabelsalvati <izabelsalvati@gmail.com>
2019-04-10 20:36:02 -0300

    upload2

---
+++ megasena/src/main/java/br/com/horacio/megasena/MegasenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.horacio.megasena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegasenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegasenaApplication.class, args);
+	}
+
+}
